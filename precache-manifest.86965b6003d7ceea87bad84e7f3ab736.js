ac290465548fb39044d1d918b53cdca1c36d76ad
Akram898 <48858363+Akram898@users.noreply.github.com>
2020-08-11 18:36:31 +0200

    u

---
+++ precache-manifest.86965b6003d7ceea87bad84e7f3ab736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9f2c23d22774a4de4165bd6180c7cd",
+    "url": "/new-react-movies/index.html"
+  },
+  {
+    "revision": "d1f2a8dbbaab2c239ef4",
+    "url": "/new-react-movies/static/css/main.31140324.chunk.css"
+  },
+  {
+    "revision": "79af6c941c517a87fe9a",
+    "url": "/new-react-movies/static/js/2.3984730b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-react-movies/static/js/2.3984730b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f2a8dbbaab2c239ef4",
+    "url": "/new-react-movies/static/js/main.dfb2d292.chunk.js"
+  },
+  {
+    "revision": "fa90a5e27c38026f882f",
+    "url": "/new-react-movies/static/js/runtime-main.748f183c.js"
+  },
+  {
+    "revision": "eb513551a480899bccab61d5a2cf4cf2",
+    "url": "/new-react-movies/static/media/bg.eb513551.jpg"
+  }
+]);
